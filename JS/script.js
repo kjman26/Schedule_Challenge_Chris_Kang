52940cf79c85593c0e96b0d03cac138092883314
Chris <kjman26@gmail.com>
2022-07-05 20:36:01 -0700

    moved index

--- JS/script.js
+++ JS/script.js
@@ -12,8 +12,3 @@ document.querySelectorAll("list-group-item") = function(){
   }
 
 };
-function fnsubmit(){
-  this.querySelectorAll("[type=hidden]").value= JSON.stringify(clickedlinks);
-  console.log(JSON.parse(this.querySelector("input[type=hidden]").value)[0]);
-  event.preventDefault();//return true;
-}
