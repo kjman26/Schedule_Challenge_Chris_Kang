c26629064915984020cd78e810a93c2a1496413d
Chris <kjman26@gmail.com>
2022-07-05 20:21:25 -0700

    date and time display

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,14 @@
-$('.datepicker').datepicker({
-    labelMonthNext: 'Go to the next month',
-    labelMonthPrev: 'Go to the previous month',
-    labelMonthSelect: 'Pick a month from the dropdown',
-    labelYearSelect: 'Pick a year from the dropdown',
-    selectMonths: true,
-    selectYears: true
-  })
+function refreshTime(){
+  const timeDisplay = document.getElementById("currentDay");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(",","-");
+  timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
+
+document.querySelectorAll("list-group-item") = function(){
+  if("list-group-item < timeDisplay"){
+    document.querySelector(".list-group-item").style.background = "red";
+  }
+
+};
